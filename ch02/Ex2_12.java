f58ea7b407a201ffc186ff529c9529e3c2c2d309
khyunji <sso01215@naver.com>
2023-01-01 12:05:30 +0900

    practice java

--- ch02/Ex2_12.java
+++ ch02/Ex2_12.java
@@ -1,19 +1,19 @@
 
 public class Ex2_12 {
 	public static void main(String[] args) {
-		String str = "3";
+
+		System.out.println(1);
+		System.out.println('2');
+		System.out.println('3'-'0'); // -'0': 숫자화
+		System.out.println('4'+'0'); // +'0': 문자화, '0' = 48, '4' = 52
+		System.out.println((char)('4'+'0')); // char 100 = d
+		System.out.println(5+'0'); // +'0': 문자화
+		System.out.println(6-'0'); // -'0': 숫자화
+		System.out.println("6");
+		System.out.println("7"+0);
+		System.out.println("7"+'0');
+		System.out.println("7"+"0");
+		System.out.println('9'+'9'); // '숫자' < '10'
 		
-		System.out.println('3' - '0');
-		//문자 '3'에서 '0'을 빼면, 숫자 3
-		System.out.println('3' - '0' +1);
-		System.out.println(3+1);
-		
-		System.out.println("3"+"1");
-		//문자열3과 문자열1의 결합
-		System.out.println(3 + '0');
-		//숫자3에 '0'을 더하면 문자 '3', 문자0은 숫자 48
-		System.out.println((char)(3+'0'));
-		//숫자3을 문자화
-		System.out.println(str);
 	}
 }
