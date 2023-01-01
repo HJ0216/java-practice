cc9d4e5bdad44ece82d2d68a522803defe20d022
khyunji <sso01215@naver.com>
2023-01-01 11:32:40 +0900

    Ex2_1 practice

--- ch02/Ex2_1.java
+++ ch02/Ex2_1.java
@@ -1,22 +1,26 @@
 
 public class Ex2_1 {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		// println: 입력값 + Enter
 		System.out.println("Hello");
 		System.out.println("Hello");
 		
+		// print: 입력값
+		// \n: Enter
 		System.out.print("\nHello, Friend");
-		System.out.print("Hello, Friend");
-		System.out.print("Hello, Friend");
+		System.out.print("Hello,\n Friend");
 		
 		System.out.println("\n"+(3+5));
 
 		int a = 22;
 		int b = 23;
+		// printf: to write a formatted string using the specified format string and arguments
+		// printf("출력값 형태1, 출력값 형태2, ...", 입력값1, 입력값2, ...)
+		// %d: 10진법 정수
 		System.out.printf("a=%d, b=%d%n", a, b);
 		
-		a = b;
-		b = a;
+		a = b; // a에 b값을 저장 a = 23
+		b = a; // b에 a값을 저장 b = 23
 		System.out.printf("a=%d, b=%d%n", a, b);
 		
 		
@@ -24,11 +28,10 @@ public class Ex2_1 {
 		int y = 14;
 		System.out.printf("x=%d, y=%d%n", x, y);
 		
-		int tmp = x;
-		x = y;
-		y = tmp;
+		int tmp = x; // tmp에 x 값을 저장, tmp = 12
+		x = y; // x에 y값을 저장, x = 14
+		y = tmp; // y에 tmp값을 저장, y = 12
 		System.out.printf("x=%d, y=%d, tmp=%d%n", x, y, tmp);
 		
 	}
-
 }
